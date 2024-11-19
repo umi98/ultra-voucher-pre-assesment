dc394b5d848aed41560e481d053c1ea8b289b4d8
umifarida98 <umifarida98@gmail.com>
2024-11-19 19:34:13 +0700

    add explanation to no2.sql

--- no2.sql
+++ no2.sql
@@ -1,3 +1,4 @@
+-- CREATING TABLE
 create table people (
 id int primary key,
 name varchar(255),
@@ -5,12 +6,14 @@ parent_id int,
 foreign key(parent_id) references people(id)
 );
 
+-- INSERT VALUE TO PEOPLE
 insert into people values
 (1, 'Zaki', 2),
 (2, 'Ilham', null),
 (3, 'Irwan', 2),
 (4, 'Arka', 3);
 
+-- QUERY TO GET DESIRED RESULT
 select p.id, p.name, p2.name as parent_name
 from people p
 left join people p2 on p.parent_id = p2.id
